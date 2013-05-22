44fd2f5ee7abb51fd148e416b278996b2bf6288c
Richard Zhang <upyoursjiveturkey@hotmail.com>
2013-05-22 21:39:19 +1000

    Fixed save button, works!!
    With collaboration with Geoffrey ofcourse

--- MiscButtonHandler.java
+++ MiscButtonHandler.java
@@ -1,9 +1,11 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 public class MiscButtonHandler implements ActionListener{
@@ -13,27 +15,34 @@ public class MiscButtonHandler implements ActionListener{
 		this.puzzle = puzzle;
 	}
 	public void actionPerformed(ActionEvent event) {
+		JFrame miscFrame = new JFrame();
+		miscFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		if (event.getActionCommand() == "Save")
 		{
-			this.puzzle.savePuzzle();
+			JLabel instruction = new JLabel("Please enter your desired save ID below");
+			JTextField inputField = new JTextField();
+			inputField.addActionListener(new SaveIDHandler(puzzle, miscFrame));
+			miscFrame.add(instruction, BorderLayout.NORTH);
+			miscFrame.add(inputField, BorderLayout.CENTER);
+			miscFrame.pack();
+			miscFrame.setVisible(true);
+			//this.puzzle.savePuzzle();
 		}
 		
 		if (event.getActionCommand() == "Check")
 		{
 			boolean correct = this.puzzle.checkPuzzle();
-			JFrame checkFrame = new JFrame();
-			checkFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			if (correct == true)
 			{
 				JLabel correctText = new JLabel("Congratulations, you solved the puzzle!");
-				checkFrame.add(correctText, BorderLayout.CENTER);
+				miscFrame.add(correctText, BorderLayout.CENTER);
 			}else
 			{
 				JLabel incorrectText = new JLabel("Looks like this puzzle is incorrect, try again");
-				checkFrame.add(incorrectText, BorderLayout.CENTER);
+				miscFrame.add(incorrectText, BorderLayout.CENTER);
 			}
-			checkFrame.pack();
-			checkFrame.setVisible(true);
+			miscFrame.pack();
+			miscFrame.setVisible(true);
 		}
 		
 	}
---
+++ SaveIDHandler.java
@@ -0,0 +1,28 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class SaveIDHandler implements ActionListener{
+	private Puzzle puzzle;
+	private JFrame parentFrame;
+	public SaveIDHandler(Puzzle puzzle, JFrame parentFrame)
+	{
+		this.puzzle = puzzle;
+		this.parentFrame = parentFrame;
+	}
+	public void actionPerformed(ActionEvent event) {
+		this.puzzle.setPuzzleName(event.getActionCommand());
+		this.puzzle.setID(event.getActionCommand());
+		this.puzzle.savePuzzle();
+		JFrame frame = new JFrame();
+		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new JLabel("Puzzle saved!"));
+		frame.pack();
+		parentFrame.setVisible(false);
+		frame.setVisible(true);
+	}
+
+}
